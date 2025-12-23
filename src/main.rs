7ab94851c2afa25cf2c5001a3f809287c106b644
massivebird <33991373+massivebird@users.noreply.github.com>
2025-12-23 15:07:02 -0500

    yay!

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,10 @@ struct Config {
 
 #[derive(Deserialize, Debug)]
 struct Dir {
+    display_name: String,
     match_dirs: bool,
+    color: [u8; 3],
+    path: String,
 }
 
 fn main() {
@@ -17,6 +20,6 @@ fn main() {
     let mut buf = String::new();
     f.read_to_string(&mut buf).unwrap();
 
-    let config: Dir = toml::from_str(&buf).unwrap();
+    let config: Config = toml::from_str(&buf).unwrap();
     dbg!(config);
 }
