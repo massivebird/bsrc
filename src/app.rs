816f462ba7ef7554b253354402a51ee30442a048
massivebird <33991373+massivebird@users.noreply.github.com>
2025-12-23 15:56:25 -0500

    added: App struct, clap

---
+++ src/app.rs
@@ -0,0 +1,56 @@
+use clap::Arg;
+use regex::Regex;
+use serde::Deserialize;
+use std::io::Read;
+
+#[derive(Debug)]
+pub struct App {
+    pub query: Regex,
+    pub config: Config,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    pub dirs: Vec<Dir>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Dir {
+    pub display_name: String,
+    pub match_dirs: bool,
+    pub color: [u8; 3],
+    pub path: String,
+}
+
+impl App {
+    pub fn build() -> Self {
+        let matches = clap::command!()
+            .arg(
+                Arg::new("query")
+                    .required(true)
+                    .hide(true)
+                    .help("Regular expression query"),
+            )
+            .get_matches();
+
+        // Shortcut for retrieving a command line argument.
+        let get_arg = |arg_name: &str| -> Option<&String> { matches.get_one::<String>(arg_name) };
+
+        let query = Regex::new(get_arg("query").unwrap()).unwrap();
+
+        let mut f = std::fs::File::open("./bsrc.toml").unwrap();
+        let mut buf = String::new();
+        f.read_to_string(&mut buf).unwrap();
+
+        let config: Config = match toml::from_str(&buf) {
+            Ok(c) => c,
+            Err(e) => {
+                panic!("{e}");
+            }
+        };
+
+        dbg!(&query);
+
+        Self { query, config }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,9 @@
-use serde::Deserialize;
-use std::io::Read;
-
-#[derive(Deserialize, Debug)]
-struct Config {
-    dirs: Vec<Dir>,
-}
-
-#[derive(Deserialize, Debug)]
-struct Dir {
-    display_name: String,
-    match_dirs: bool,
-    color: [u8; 3],
-    path: String,
-}
+mod app;
 
 fn main() {
-    let mut f = std::fs::File::open("./bsrc.toml").unwrap();
-    let mut buf = String::new();
-    f.read_to_string(&mut buf).unwrap();
+    let app = app::App::build();
 
-    let config: Config = match toml::from_str(&buf) {
-        Ok(c) => c,
-        Err(e) => {
-            panic!("{e}");
-        }
-    };
-
-    for dir in &config.dirs {
-        println!("{}", dir.display_name);
-    }
+    // for dir in &config.dirs {
+    //     println!("{}", dir.display_name);
+    // }
 }
