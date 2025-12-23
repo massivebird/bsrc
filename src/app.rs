2458a24a01af78041ca5244759104353ad8341da
massivebird <33991373+massivebird@users.noreply.github.com>
2025-12-23 16:14:59 -0500

    feat: functional queries

--- src/app.rs
+++ src/app.rs
@@ -2,19 +2,21 @@ use clap::Arg;
 use regex::Regex;
 use serde::Deserialize;
 use std::io::Read;
+use std::path::PathBuf;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct App {
+    pub root: PathBuf,
     pub query: Regex,
     pub config: Config,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Clone, Debug)]
 pub struct Config {
     pub dirs: Vec<Dir>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Clone, Debug)]
 pub struct Dir {
     pub display_name: String,
     pub match_dirs: bool,
@@ -25,21 +27,47 @@ pub struct Dir {
 impl App {
     pub fn build() -> Self {
         let matches = clap::command!()
-            .arg(
+            .args([
                 Arg::new("query")
                     .required(true)
                     .hide(true)
                     .help("Regular expression query"),
-            )
+                Arg::new("all")
+                    .short('a')
+                    .long("all")
+                    .required(false)
+                    .conflicts_with("query")
+                    .action(clap::ArgAction::SetTrue)
+                    .help("Display all files"),
+                Arg::new("case_sensitive")
+                    .long("case-sensitive")
+                    .action(clap::ArgAction::SetTrue)
+                    .help("Execute query case sensitively"),
+            ])
             .get_matches();
 
         // Shortcut for retrieving a command line argument.
         let get_arg = |arg_name: &str| -> Option<&String> { matches.get_one::<String>(arg_name) };
 
-        let query = Regex::new(get_arg("query").unwrap()).unwrap_or_else(|e| {
-            eprintln!("{e}");
-            std::process::exit(1);
-        });
+        let query: Regex = {
+            let raw_query = if matches.get_flag("all") {
+                "."
+            } else {
+                get_arg("query").unwrap()
+            };
+
+            // Default to case-insensitive
+            let opts = if matches.get_flag("case_sensitive") {
+                ""
+            } else {
+                "(?i)"
+            };
+
+            Regex::new(&format!("{opts}{raw_query}")).unwrap_or_else(|e| {
+                eprintln!("{e}");
+                std::process::exit(1);
+            })
+        };
 
         let mut f = std::fs::File::open("./bsrc.toml").unwrap();
         let mut buf = String::new();
@@ -52,8 +80,10 @@ impl App {
             }
         };
 
-        dbg!(&query);
-
-        Self { query, config }
+        Self {
+            query,
+            config,
+            root: PathBuf::from("/home/penguino/game-archive/"),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,74 @@
+use std::collections::VecDeque;
+use std::path::Path;
+
+use self::app::{App, Dir};
+
 mod app;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let app = app::App::build();
 
-    // for dir in &config.dirs {
-    //     println!("{}", dir.display_name);
-    // }
+    let mut handles = VecDeque::new();
+
+    for system in app.config.dirs.clone() {
+        let app = app.clone();
+        handles.push_back(tokio::spawn(async move { query_system(&app, system) }));
+    }
+
+    let mut num_matches: u32 = 0;
+
+    for system in app.config.dirs {
+        let matches = handles.pop_front().unwrap().await.unwrap();
+
+        num_matches += u32::try_from(matches.len()).unwrap();
+
+        for m in matches {
+            println!("{m}");
+        }
+    }
+
+    println!(
+        "{num_matches} {noun} found.",
+        noun = match num_matches {
+            1 => "match",
+            _ => "matches",
+        }
+    );
+}
+
+fn query_system(app: &App, dir: Dir) -> Vec<String> {
+    let mut matches: Vec<String> = Vec::new();
+
+    let system_path = app.root.join(dir.path);
+
+    for entry in Path::new(&system_path)
+        .read_dir()
+        .unwrap()
+        .filter_map(Result::ok)
+    // .filter(is_not_bios_dir)
+    {
+        if dir.match_dirs && entry.path().is_file() || !dir.match_dirs && entry.path().is_dir() {
+            continue;
+        }
+
+        let path = &entry.path();
+
+        let filename = {
+            let Some(stem) = path.file_stem() else {
+                panic!("Failed to parse filename from path: {}", path.display());
+            };
+
+            stem.to_string_lossy()
+        };
+
+        // TODO: Apply cleaning.
+        // e.g. "Pokemon Snap (USA).n64" -> "Pokemon Snap"
+
+        if app.query.is_match(&filename) {
+            matches.push(filename.to_string());
+        }
+    }
+
+    matches
 }
