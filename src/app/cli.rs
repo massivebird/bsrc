4ef9967863ef42474fd3e0ab08d22dfc6f6806c6
massivebird <33991373+massivebird@users.noreply.github.com>
2025-12-23 23:40:48 -0500

    changed: help messages, formatting

--- src/app/cli.rs
+++ src/app/cli.rs
@@ -1,5 +1,21 @@
 use clap::Arg;
 
+static QUERY_LONG_HELP: &str = "\
+A directory to start searching from. Defaults to the current directory.
+
+Must contain a valid `bsrc.toml` at its root.";
+
+static ONLY_LONG_HELP: &str = "\
+Only searches directories with the provided IDs. In the TOML config, directory IDs are in each directory header: `[dirs.<id>]`.
+
+Examples:
+
+# Only search directory with ID \"gba\".
+bsrc --only gba \"metal\"
+
+# Supports multiple comma-separated IDs.
+bsrc --only gba,snes,ds \"metal\"";
+
 pub fn build() -> clap::Command {
     clap::command!()
         .next_help_heading("Positional arguments")
@@ -8,11 +24,9 @@ pub fn build() -> clap::Command {
                 .required(true)
                 .value_name("PATTERN")
                 .help("A regular expression query."),
-            Arg::new("root").value_name("PATH").long_help(
-                "A directory to start searching from. Defaults to the current directory.
-
-Must contain a valid `bsrc.toml` at its root.",
-            ),
+            Arg::new("root")
+                .value_name("PATH")
+                .long_help(QUERY_LONG_HELP),
         ])
         .next_help_heading("Search options")
         .args([
@@ -22,7 +36,8 @@ Must contain a valid `bsrc.toml` at its root.",
                 .required(false)
                 .conflicts_with("query")
                 .action(clap::ArgAction::SetTrue)
-                .help("Display all files"),
+                .help("Display all files")
+                .long_help("Displays all files. Equivalent to the regex query \".\""),
             Arg::new("case_sensitive")
                 .long("case-sensitive")
                 .action(clap::ArgAction::SetTrue)
@@ -32,14 +47,6 @@ Must contain a valid `bsrc.toml` at its root.",
                 .long("only")
                 .value_name("IDs")
                 .help("Only search directories specified by ID.")
-                .long_help("Only searches directories with the provided IDs. In the TOML config, directory IDs are in each directory header: `[dirs.<id>]`.
-
-Examples:
-
-# Only search directory with ID \"gba\".
-bsrc --only gba \"metal\"
-
-# Supports multiple comma-separated IDs.
-bsrc --only gba,snes,ds \"metal\""),
+                .long_help(ONLY_LONG_HELP),
         ])
 }
